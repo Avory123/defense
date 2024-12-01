e117904027d9b20d133abdd814b0c6ca729527c9
Kafka <101793547+Avory123@users.noreply.github.com>
2024-12-01 23:09:40 +0800

    Add files via upload

---
+++ IT Proj/Login/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("navigateButton").onclick = function () {
+        window.location.href = "/Updated/index.html";
+    };
+});
---
+++ IT Proj/Profile/script.js
@@ -0,0 +1,6 @@
+document.getElementById("navigateButton").onclick = function () {
+    window.location.href = "/UI/index.html";
+    alert("Logging Out... Going Back to the Main Page")
+  };
+
+  
