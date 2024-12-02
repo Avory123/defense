bf2e044526773c1a451942764b3e60881863b23d
Kafka <101793547+Avory123@users.noreply.github.com>
2024-12-02 09:22:00 +0800

    Delete IT Proj directory

--- IT Proj/About us/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("navigateButton").onclick = function () {
-    window.location.href = "/UI/index.html";
-    alert("Logging Out... Going Back to the Main Page")
-  };
-
-  
--- IT Proj/Login/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("navigateButton").onclick = function () {
-        window.location.href = "/Updated/index.html";
-    };
-});
